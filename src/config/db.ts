2e35cf28ca878269d7ee553840d56055f8a2c8e8
Akhand0ps <akhandps08@gmail.com>
2025-12-29 00:03:58 +0530

    feat: add MongoDB connection and dotenv configuration

---
+++ src/config/db.ts
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+
+
+async function connectdb(uri:string):Promise<void>{
+
+    try{
+        const conn =  await mongoose.connect(uri);
+        console.log(`MongoDB Connected => ${conn.connection.host}`)
+    }catch(err:unknown){
+
+        if(err instanceof Error){
+            console.error('Mongo db connection failed => ',err.message);
+        }
+        else{
+            console.log('An unknown error occured');
+        }
+    }
+}
+
+
+export default connectdb;
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
 import {Request,Response} from "express";
+import dotenv from "dotenv";
+dotenv.config();
 import app from "./app.js"
+import connectdb from "./config/db.js";
 
 
 
 const PORT:Number = 3000;
+const uri:string = process.env.uri ??' ';
 
+// console.log("uri:",uri);
 
 app.get("/",(req:Request,res:Response)=>{
 
@@ -15,8 +20,17 @@ app.get("/health",(req:Request,res:Response)=>{
     res.send('all good');
 })
 
+connectdb(uri)
+    .then(()=>{
+        app.listen(PORT,()=>{
+        console.log(`server running on ${PORT}`)
+        })
+    })
+    .catch((err:unknown)=>{
 
-app.listen(PORT,()=>{
+        if(err instanceof Error) console.error(err.message)
+        else console.error('Error came connecting db and starting server');
+    })
 
-    console.log(`server running on ${PORT}`)
-})
+
+
